f616fb9b5c31c6b63f45f3486715830ae641fa20
Bhagyashree <p.bhagyashree992@gmail.com>
2024-05-25 17:39:57 -0400

    updated okta uri

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,9 +1,10 @@
 export const oktaConfig =  {
     clientId:"0oagm790zxWFw78ws5d7",
     issuer:"https://dev-53499801.okta.com/oauth2/default",
-    redirectUri:`http://localhost:3000/login/callback`,
+    redirectUri:`http://18.189.11.223/login/callback`,
     scopes:['openid' , 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true,
     useClassicEngine: true
 }
+
