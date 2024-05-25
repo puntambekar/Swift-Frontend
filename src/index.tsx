f6f8348faad85d211476dd19ec917cf62f2c253d
Bhagyashree <p.bhagyashree992@gmail.com>
2024-05-25 16:40:52 -0400

    added redirect file and hashrouter in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,17 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import { App } from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from "react-router-dom";
+
 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
+  <Router>
     <App />
-  </BrowserRouter>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
