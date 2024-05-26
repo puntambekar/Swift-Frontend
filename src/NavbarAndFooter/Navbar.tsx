5f90e8cea41aae5ecebe28174148dfc6e2e71a05
Bhagyashree <p.bhagyashree992@gmail.com>
2024-05-26 16:32:15 -0400

    changed anchor tag to  Navlink in navbar

--- src/NavbarAndFooter/Navbar.tsx
+++ src/NavbarAndFooter/Navbar.tsx
@@ -80,10 +80,10 @@ export const Navbar = () => {
                                             <button className="nav-link link-body-emphasis px-2" role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{ border: "1px solid black", borderRadius: "20px", padding: "2px" }}>
                                                 <b >{user?.firstName}</b><i className="bi bi-caret-down-fill" style={{ marginLeft: "8px", fontWeight: "bolder" }}></i></button>
                                             <ul className="dropdown-menu"style={{ borderRadius: "10px" }}>
-                                                <li><a className="dropdown-item" href="/backoffice/profile">Profile</a></li>
-                                                <li><a className="dropdown-item" href="/backoffice/bookings">Retrieve Bookings</a></li>
-                                                <li><a className="dropdown-item" href="/backoffice/manage">Manage Bookings</a></li>
-                                                <li><a className="dropdown-item" href="/backoffice/availability">Manage Availability</a></li>
+                                                <NavLink className="dropdown-item" to="/backoffice/profile">Profile</NavLink>
+                                                <NavLink className="dropdown-item" to="/backoffice/bookings">Retrieve Bookings</NavLink>
+                                                <NavLink className="dropdown-item" to="/backoffice/manage">Manage Bookings</NavLink>
+                                                <NavLink className="dropdown-item" to="/backoffice/availability">Manage Availability</NavLink>
                                                 <li><button className="dropdown-item" onClick={handleLogout}>Logout</button></li>
                                             </ul>
                                         </li></>
@@ -93,7 +93,7 @@ export const Navbar = () => {
                                                 <b >{user?.firstName}</b><i className="bi bi-caret-down-fill" style={{ marginLeft: "8px", fontWeight: "bolder" }}></i></button>
                                             <ul className="dropdown-menu" style={{ borderRadius: "10px" }}>
 
-                                                <li><a className="dropdown-item" href="/user/manage">Manage Bookings</a></li>
+                                                <NavLink className="dropdown-item" to="/user/manage">Manage Bookings</NavLink>
                                                 <li><button className="dropdown-item" onClick={handleLogout}>Logout</button></li>
 
                                             </ul>
