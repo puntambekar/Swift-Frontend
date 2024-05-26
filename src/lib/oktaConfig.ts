3161e8bdf64a0f50c4aa038d499e26b3eefa6273
Bhagyashree <p.bhagyashree992@gmail.com>
2024-05-26 16:02:56 -0400

    updated okta config

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig =  {
     clientId:"0oagm790zxWFw78ws5d7",
     issuer:"https://dev-53499801.okta.com/oauth2/default",
-    redirectUri:`http://18.189.11.223/login/callback`,
+    redirectUri:`https://swift-frontend-n9xl.onrender.com/login/callback`,
     scopes:['openid' , 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true,
