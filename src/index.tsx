a6b4f00dff424ccd080cb5680d60337af0e1b74d
Bhagyashree <p.bhagyashree992@gmail.com>
2024-05-26 16:46:06 -0400

    Changed hashRouter to browserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import { App } from './App';
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 
 
 
@@ -12,9 +12,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <Router>
+  <BrowserRouter>
     <App />
-  </Router>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
